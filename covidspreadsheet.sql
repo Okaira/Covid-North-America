13262ab08f92b635cd18d30a34ad13452da0d63a
Opa Kaira <116534743+Okaira@users.noreply.github.com>
2023-10-07 08:20:44 -0400

    Delete covidspreadsheet.sql

--- covidspreadsheet.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE covid19 (
-  ObservationDate DATE,
-  ProvinceState VARCHAR(255),
-  CountryRegion VARCHAR(255),
-  LastUpdate TIMESTAMP,
-  Confirmed INT,
-  Deaths INT,
-  Recovered INT
-);
